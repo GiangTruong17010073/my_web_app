4f88832d732d801e3af727a783721d5d2554510d
GiangTruong17010073 <giang.truong170173@vnuk.edu.vn>
2019-03-05 22:44:46 +0700

    Assignment

--- scripts/studies.js
+++ scripts/studies.js
@@ -16,4 +16,17 @@ window.onload = function () {
       this.classList.add('highlighted');
     });
   }
+
+  for(var i = 0; i < trs.length; i++){
+    trs[i].addEventListener('mouseover', function(event){
+      event.preventDefault();
+
+      var highlighteds = document.getElementsByClassName('highlighted');
+
+      for(var i = highlighteds.length - 1;i >= 0; i--){
+        highlighteds[i].classList.remove('highlighted');
+      }
+      this.classList.add('highlighted');
+    });
+  }
 }
