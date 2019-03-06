6c051dad07afdfa5c5a455655430e24b08dadc65
GiangTruong17010073 <giang.truong170173@vnuk.edu.vn>
2019-03-06 10:37:43 +0700

    some Changes

--- scripts/jquery/index.js
+++ scripts/jquery/index.js
@@ -1,8 +1,9 @@
-$(window).resize(logScreenSize);
+//$(window).resize(logScreenSize);
 
 
 $(function(){
-  logScreenSize();
+  //logScreenSize();
+  resizeElements(['.my-button-people-name','.my-button-jump-to-people']);
 });
 
 
@@ -34,7 +35,26 @@ function logScreenSize(){
 
   }
 
-
   console.log(`The size of your screen is ${size}`);
 
+}
+
+
+function resizeElements(elements){
+
+  $.each(elements, function(index, element){
+    var width = 0;
+
+    $(element).css('width', 'auto');
+
+    $(element).each(function(){
+      var thisWidth = parseFloat($(this).css('width'));
+      if(thisWidth > width){
+        width = thisWidth;
+      }
+    });
+
+    $(element).css('width', width);
+
+  });
 }
